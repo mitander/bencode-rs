3cf5293a86d6aa481bb57f4d1e95a3d0765aade1
mitander <carlmitander@gmail.com>
2023-08-09 16:05:14 +0200

    Parse bytes

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,24 @@ use std::{collections::HashMap, fmt::Debug};
 
 use nom::{
     branch::alt,
+    bytes::complete::take,
     character::complete::{char, digit1},
+    combinator::recognize,
     error::{ErrorKind, ParseError},
-    sequence::{delimited, pair},
+    multi::many_till,
+    sequence::{delimited, pair, preceded},
     IResult,
 };
 
 #[derive(Debug)]
-pub enum BenError<I> {
+pub enum BencodeError<I> {
     Nom(I, ErrorKind),
     InvalidInteger(I),
     ParseIntError(I, ParseIntError),
+    InvalidBytesLength(I),
 }
 
-impl<I> ParseError<I> for BenError<I> {
+impl<I> ParseError<I> for BencodeError<I> {
     fn from_error_kind(input: I, kind: nom::error::ErrorKind) -> Self {
         Self::Nom(input, kind)
     }
@@ -25,16 +29,16 @@ impl<I> ParseError<I> for BenError<I> {
     }
 }
 
-impl<I> From<BenError<I>> for nom::Err<BenError<I>> {
-    fn from(value: BenError<I>) -> Self {
+impl<I> From<BencodeError<I>> for nom::Err<BencodeError<I>> {
+    fn from(value: BencodeError<I>) -> Self {
         match value {
-            value @ BenError::Nom(_, _) => Self::Error(value),
+            value @ BencodeError::Nom(_, _) => Self::Error(value),
             value => Self::Failure(value),
         }
     }
 }
 
-type BenResult<'a> = IResult<&'a [u8], Value<'a>, BenError<&'a [u8]>>;
+type BencodeResult<'a> = IResult<&'a [u8], Value<'a>, BencodeError<&'a [u8]>>;
 
 #[derive(Debug, Clone)]
 pub enum Value<'a> {
@@ -45,7 +49,7 @@ pub enum Value<'a> {
 }
 
 impl<'a> Value<'a> {
-    fn parse_integer(input: &'a [u8]) -> BenResult {
+    fn parse_integer(input: &'a [u8]) -> BencodeResult {
         let (next, item) = delimited(
             char('i'),
             alt((
@@ -59,29 +63,43 @@ impl<'a> Value<'a> {
         let str = std::str::from_utf8(item).expect("value should be a valid integer string");
 
         if str.starts_with("-0") || (str.starts_with('0') && str.len() > 1) {
-            Err(nom::Err::Failure(BenError::InvalidInteger(input)))?
+            Err(nom::Err::Failure(BencodeError::InvalidInteger(input)))?
         }
 
-        let item: i64 = str.parse().map_err(|e| BenError::ParseIntError(next, e))?;
+        let item: i64 = str
+            .parse()
+            .map_err(|e| BencodeError::ParseIntError(next, e))?;
         Ok((next, Value::Integer(item)))
     }
 
-    fn parse_bytes(input: &'a [u8]) -> BenResult<'a> {
+    fn parse_bytes(input: &'a [u8]) -> BencodeResult<'a> {
+        let (input, length) = digit1(input)?;
+        let (input, _) = char(':')(input)?;
+
+        let length = std::str::from_utf8(length).expect("value should be a valid integer string");
+        let length: u64 = length
+            .parse()
+            .map_err(|e| BencodeError::ParseIntError(input, e))?;
+
+        if length == 0 {
+            Err(BencodeError::InvalidBytesLength(input))?;
+        }
+
+        let (next, item) = take(length)(input)?;
+        Ok((next, Value::Bytes(item)))
+    }
+
+    fn parse_list(input: &'a [u8]) -> BencodeResult<'a> {
         !todo!()
     }
 
-    fn parse_list(input: &'a [u8]) -> BenResult<'a> {
-        !todo!()
-    }
-
-    fn parse_dict(input: &'a [u8]) -> BenResult<'a> {
+    fn parse_dict(input: &'a [u8]) -> BencodeResult<'a> {
         !todo!()
     }
 }
-
 #[cfg(test)]
 mod tests {
-    use crate::{BenError, Value};
+    use crate::{BencodeError, Value};
     use assert_matches::assert_matches;
 
     #[test]
@@ -102,21 +120,60 @@ mod tests {
     #[test]
     fn parse_integer_errors() {
         let v = Value::parse_integer(b"i-0e").unwrap_err();
-        assert_matches!(v, nom::Err::Failure(BenError::InvalidInteger(_)));
+        assert_matches!(v, nom::Err::Failure(BencodeError::InvalidInteger(_)));
 
         let v = Value::parse_integer(b"i00e").unwrap_err();
-        assert_matches!(v, nom::Err::Failure(BenError::InvalidInteger(_)));
+        assert_matches!(v, nom::Err::Failure(BencodeError::InvalidInteger(_)));
 
         let v = Value::parse_integer(b"i-00e").unwrap_err();
-        assert_matches!(v, nom::Err::Failure(BenError::InvalidInteger(_)));
+        assert_matches!(v, nom::Err::Failure(BencodeError::InvalidInteger(_)));
 
         let v = Value::parse_integer(b"i01e").unwrap_err();
-        assert_matches!(v, nom::Err::Failure(BenError::InvalidInteger(_)));
+        assert_matches!(v, nom::Err::Failure(BencodeError::InvalidInteger(_)));
 
         let v = Value::parse_integer(b"i0123e").unwrap_err();
-        assert_matches!(v, nom::Err::Failure(BenError::InvalidInteger(_)));
+        assert_matches!(v, nom::Err::Failure(BencodeError::InvalidInteger(_)));
 
         let v = Value::parse_integer(b"l1i2ee").unwrap_err();
-        assert_matches!(v, nom::Err::Error(BenError::Nom(..)));
+        assert_matches!(v, nom::Err::Error(BencodeError::Nom(..)));
+    }
+
+    #[test]
+    fn parse_bytes() {
+        let (_, v) = Value::parse_bytes(b"2:qt").unwrap();
+        assert_matches!(v, Value::Bytes(b"qt"));
+
+        let (_, v) = Value::parse_bytes(b"2:rust").unwrap();
+        assert_matches!(v, Value::Bytes(b"ru"));
+
+        let (_, v) = Value::parse_bytes(b"3:joker").unwrap();
+        assert_matches!(v, Value::Bytes(b"jok"));
+
+        let (_, v) = Value::parse_bytes(b"4:forest").unwrap();
+        assert_matches!(v, Value::Bytes(b"fore"));
+    }
+
+    #[test]
+    fn parse_bytes_errors() {
+        let v = Value::parse_bytes(b"error").unwrap_err();
+        assert_matches!(v, nom::Err::Error(BencodeError::Nom(..)));
+
+        let v = Value::parse_bytes(b"x:error").unwrap_err();
+        assert_matches!(v, nom::Err::Error(BencodeError::Nom(..)));
+
+        let v = Value::parse_bytes(b"7:error").unwrap_err();
+        assert_matches!(v, nom::Err::Error(BencodeError::Nom(..)));
+
+        let v = Value::parse_bytes(b"-1:error").unwrap_err();
+        assert_matches!(v, nom::Err::Error(BencodeError::Nom(..)));
+
+        let v = Value::parse_bytes(b"-0:error").unwrap_err();
+        assert_matches!(v, nom::Err::Error(BencodeError::Nom(..)));
+
+        let v = Value::parse_bytes(b"00:error").unwrap_err();
+        assert_matches!(v, nom::Err::Failure(BencodeError::InvalidBytesLength(_)));
+
+        let v = Value::parse_bytes(b"0:error").unwrap_err();
+        assert_matches!(v, nom::Err::Failure(BencodeError::InvalidBytesLength(_)));
     }
 }
